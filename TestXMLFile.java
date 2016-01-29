76ef3b4ae000fb40d7a98728051f1d81b3f4a782
Felipe Barros <felipebarros.ti@gmail.com>
2016-01-29 16:49:11 -0200

    Create TestXMLFile.java

---
+++ TestXMLFile.java
@@ -0,0 +1,22 @@
+package br.com.fbarros.test;
+
+public class TestXMLFile{
+
+	/*
+	 *   -- Classe de Teste da classe XML
+	 */
+	public statis void main(String[] args){
+	
+		public static String testParentDirectoryReturnedIsValid(){
+		//write test here
+		}
+	
+		public static String testFileNameReturnedIsValid(){
+		//write test here
+		}
+		
+		public static String testFilePathReturnedIsValid(){
+		 //write test here
+		}
+	}
+}
